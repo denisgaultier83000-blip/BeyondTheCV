b20c0407693978926212a8bfa69145383cb4b413
pimpampoum <denis.gaultier83000@gmail.com>
2026-06-26 14:20:45 +0200

    fix: update import statement for database module to use relative path

--- backend/security.py
+++ backend/security.py
@@ -8,7 +8,7 @@ from fastapi.security import OAuth2PasswordBearer
 from fastapi import Depends, HTTPException
 from jose import jwt
 from passlib.context import CryptContext
-from database import db # [FIX] Import manquant qui cause des crashs sur les routes protégées
+from .database import db # [FIX] Import manquant qui cause des crashs sur les routes protégées
 
 # Configuration
 SECRET_KEY = os.getenv("SECRET_KEY", "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7")
