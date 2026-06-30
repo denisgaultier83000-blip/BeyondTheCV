c0d447ccd2404547f7595c600eb486d1ac9d985a
pimpampoum <denis.gaultier83000@gmail.com>
2026-06-30 21:48:45 +0200

    fix: Correct import statement for jwtDecode in AdminProtectedRoute

--- front/src/components/AdminProtectedRoute.tsx
+++ front/src/components/AdminProtectedRoute.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-import { jwtDecode } from 'jwt-decode';
+import jwtDecode from 'jwt-decode';
 
 interface DecodedToken {
   exp: number;
